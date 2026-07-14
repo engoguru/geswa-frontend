53fbc9ff34e2c54fa0dcbc78fd4a7f73cc0fdc50
engoguru <engoguru@carkverma.com>
2026-07-14 10:58:26 +0530

    nnew  urrl

--- src/App.jsx
+++ src/App.jsx
@@ -49,9 +49,18 @@ const dispatch=useDispatch()
 
   // verifiedUser
   // loading
-  if (!authChecked || isloginLoading) {
-    return <div>Loading...</div>;
-  }
+if (!authChecked || isloginLoading) {
+  return (
+    <div className="p-6 space-y-4">
+      <div className="h-8 w-48 bg-gray-200 rounded animate-pulse"></div>
+
+      <div className="h-40 w-full bg-gray-200 rounded-lg animate-pulse"></div>
+
+      <div className="h-4 w-full bg-gray-200 rounded animate-pulse"></div>
+      <div className="h-4 w-3/4 bg-gray-200 rounded animate-pulse"></div>
+    </div>
+  );
+}
 
   return (
     <>
--- src/utils/baseUrl.js
+++ src/utils/baseUrl.js
@@ -1,1 +1,3 @@
-export const base_Url="http://localhost:5000/api/"
+// export const base_Url="http://localhost:5000/api/"
+
+export const base_Url="https://api.geswango.in/api/"
